674670960d517667050f7fefc19754ed833718ca
Tomas Novotny <tomas@novotny.cz>
2016-06-13 21:27:06 +0000

    rover: better thread, exit and card handling
    Signed-off-by: Tomas Novotny <tomas@novotny.cz>

--- rover.py
+++ rover.py
@@ -9,6 +9,7 @@
 
 import atexit
 import glob
+import os
 import struct
 import sys
 import threading
@@ -17,6 +18,7 @@ import time
 event_path = "/dev/input/event2"
 lcd_path = "/dev/lcd0"
 lcd_buffer = ''
+running = True
 
 # long int, long int, unsigned short, unsigned short, unsigned int
 FORMAT = 'llHHI'
@@ -54,69 +56,92 @@ def update_lcd (amount, direction):
 
 def lcd_worker ():
 	global lcd_buffer, repaint_lcd
-	while (True):
+	while (running):
 		repaint_lcd.wait()
 		repaint_lcd.clear()
-		print "repainting"
+		print "Repainting"
 		lcd = open(lcd_path, 'w')
 		lcd.write(lcd_buffer)
 		lcd.close()
+	print 'Finishing lcd writer'
 
 def card_worker ():
 	global lcd_buffer, repaint_lcd
-	while (True):
+	while (running):
+		lcd_buffer_old = lcd_buffer
 		files = glob.glob('/media/*/rover.txt')
 		if (len(files) > 0):
 			with open(files[0]) as f:
 				lcd_buffer = f.read()
+		else:
+			lcd_buffer = 'Please insert a memory card.'
+		if lcd_buffer != lcd_buffer_old:
 			repaint_lcd.set()
-		time.sleep(1)
+		time.sleep(0.1)
+	print 'Finishing card reader'
 
 def goodbye ():
-	#t.join() # XXX
+	global lcd_buffer, running
+	print "Cleaning up"
+	running = False
+	card_thread.join()
+	lcd_buffer = '{:16s}{:16s}'.format('System was', 'stopped.')
+	repaint_lcd.set()
+	t.join()
 	event_file.close()
 
-atexit.register(goodbye)
-
-init_lcd ()
-
 t = threading.Thread(target=lcd_worker)
-t.daemon = True # XXX Do stopping in a better way
 repaint_lcd = threading.Event()
-t.start()
-repaint_lcd.set()
-
 card_thread = threading.Thread(target=card_worker)
-card_thread.daemon = True # XXX Do stopping in a better way
-card_thread.start()
 
-output = 0
+def main ():
+	global event_file
 
-# open file in binary mode
-event_file = open(event_path, "rb")
-event = event_file.read(EVENT_SIZE)
+	atexit.register(goodbye)
+	init_lcd ()
 
-while event_file:
-	(tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
+	t.start()
+	repaint_lcd.set()
+	card_thread.start()
 
-	direction = Dir.UNKNOWN
+	output = 0
 
-	if (type != 0 or code != 0) and value != 0:
-		if code == 113:
-			print("pushed")
-			direction = Dir.STOP
-			output = 0
-		elif code == 114:
-			print("down")
-			direction = Dir.DOWN
-			output -= 1
-		elif code == 115:
-			print("up")
-			direction = Dir.UP
-			output += 1
-		else:
-			print("unsupported code")
-
-		update_lcd(output, direction)
-
+	# open file in binary mode
+	event_file = open(event_path, "rb")
 	event = event_file.read(EVENT_SIZE)
+
+	while event_file:
+		(tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
+
+		direction = Dir.UNKNOWN
+
+		if (type != 0 or code != 0) and value != 0:
+			if code == 113:
+				print("pushed")
+				direction = Dir.STOP
+				output = 0
+			elif code == 114:
+				print("down")
+				direction = Dir.DOWN
+				output -= 1
+			elif code == 115:
+				print("up")
+				direction = Dir.UP
+				output += 1
+			else:
+				print("unsupported code")
+
+			update_lcd(output, direction)
+
+		event = event_file.read(EVENT_SIZE)
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		print 'Interrupted'
+        try:
+		goodbye()
+		sys.exit(0)
+	except SystemExit:
+		os._exit(0)

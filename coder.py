d91151ce0a47157de70677d1d50b2efeb9201897
Tomas Novotny <tomas@novotny.cz>
2016-06-22 09:23:14 +0000

    coder: we won't add random stuff
    Signed-off-by: Tomas Novotny <tomas@novotny.cz>

--- coder.py
+++ coder.py
@@ -7,14 +7,12 @@ key = 0xcafe
 
 def encode(text):
     output = ''
-    # TODO add some random stuff
     for i, c in enumerate(text):
         output += str(bin((ord(c) + i + 42) ^ key)) + "\n"
     return output
 
 def decode(text):
     output = ''
-    # TODO remove some random stuff
     for i, line in enumerate(text.splitlines()):
         output += chr((int(line, 2) ^ key) - i - 42)
     return output
